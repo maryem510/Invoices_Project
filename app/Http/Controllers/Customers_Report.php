8d3112f154fa3199c45a89233ee7fc8acf3d6941
Maryem Mohammed <Maryem201910@gmail.com>
2022-07-03 20:45:47 +0200

    Initial commit

---
+++ app/Http/Controllers/Customers_Report.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\sections;
+use App\invoices;
+
+class Customers_Report extends Controller
+{
+    public function index()
+    {
+
+        $sections = sections::all();
+        return view('reports.customers_report', compact('sections'));
+    }
+
+
+    public function Search_customers(Request $request)
+    {
+
+        if ($request->Section && $request->product && $request->start_at == '' && $request->end_at == '') {
+
+
+            $invoices = invoices::select('*')->where('section_id', '=', $request->Section)->where('product', '=', $request->product)->get();
+            $sections = sections::all();
+            return view('reports.customers_report', compact('sections'))->withDetails($invoices);
+        }
+
+
+        else {
+
+            $start_at = date($request->start_at);
+            $end_at = date($request->end_at);
+
+            $invoices = invoices::whereBetween('invoice_Date', [$start_at, $end_at])->where('section_id', '=', $request->Section)->where('product', '=', $request->product)->get();
+            $sections = sections::all();
+            return view('reports.customers_report', compact('sections'))->withDetails($invoices);
+        }
+    }
+}
